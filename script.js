20c8bd7c61c615dddacb999509b070eefd041759
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-10 15:55:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function volume_sphere() {
-    //Write your code here
-  
+let btn = document.getElementById("submit")
+let radius = document.getElementById("radius")
+let volume = document.getElementById("volume")
+let myForm = document.getElementById("MyForm")
+
+
+myForm.addEventListener("submit", ()=>{
+
+	volume_sphere()
+})
+const pi = 3.14;
+
+function volume_sphere(e) {	
+	e.preventDefault();
+	if(radius.value === ""){
+	volume.value = ""
+	return;
+}
+	
+let radiusVal = Number(radius.value);
+	
+if(radiusVal < 0){
+	volume.value = "NaN"
+}
+else{
+		let calculatedVolume = (4/3) * pi * radiusVal ** 3
+		volume.value = calculatedVolume.toFixed(4)
+	}
+
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
